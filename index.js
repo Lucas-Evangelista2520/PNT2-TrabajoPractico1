8d88055647308afa5e46d716e080b4b091e15147
lucas evangelista <lucasevangelista2520@gmail.com>
2025-09-04 13:45:58 -0300

    Parte 1 Acceso a elementos y recorrido con arrays

---
+++ index.js
@@ -0,0 +1,26 @@
+const productos = [
+    { id: 1, nombre: "Remera", precio: 1500, categoria: "Ropa" },
+    { id: 2, nombre: "Pantalón", precio: 2500, categoria: "Ropa" },
+    { id: 3, nombre: "Zapatillas", precio: 8000, categoria: "Calzado" },
+    { id: 4, nombre: "Gorra", precio: 1200, categoria: "Accesorios" },
+    { id: 5, nombre: "Campera", precio: 9500, categoria: "Ropa" }
+    ];
+
+// 1
+console.log("Nombre del primer producto:", productos[0].nombre);
+
+// 2
+console.log("\n--- Lista de productos ---");
+for (const producto of productos) {
+    console.log(`Producto: ${producto.nombre} - Precio: $${producto.precio}`);
+}
+
+// 3
+console.log("\n--- Lista de productos (usando forEach) ---");
+productos.forEach(producto => {
+    console.log(`Producto: ${producto.nombre} - Precio: $${producto.precio}`);
+});
+
+
+
+    
